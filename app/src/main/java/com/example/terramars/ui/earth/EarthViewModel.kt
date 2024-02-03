c53a42162f6e880ff624c4563ff2380023c15f5d
Kader-MVV <146785374+Kader-MVV@users.noreply.github.com>
2024-02-03 23:34:49 +0100

    Image Satellite de la Terre front

--- app/src/main/java/com/example/terramars/ui/earth/EarthViewModel.kt
+++ app/src/main/java/com/example/terramars/ui/earth/EarthViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class EarthViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Eerth Fragment"
+        value = "La Terre par Satellite"
     }
     val text: LiveData<String> = _text
 }
