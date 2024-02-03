6707147f2f48f5a96fe599d0af8dc8d12050ac78
Kader-MVV <146785374+Kader-MVV@users.noreply.github.com>
2024-02-04 00:07:29 +0100

    Mise a jour du front pour APOD et EARTH

--- app/src/main/java/com/example/terramars/ui/earth/EarthFragment.kt
+++ app/src/main/java/com/example/terramars/ui/earth/EarthFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +15,12 @@ import com.example.terramars.databinding.FragmentEarthBinding
 class EarthFragment : Fragment() {
 
     private var _binding: FragmentEarthBinding? = null
+    private lateinit var latitudeEditText: EditText
+    private lateinit var longitudeEditText: EditText
+    private lateinit var dateEditText: EditText
+    private lateinit var fetchImageButton: Button
+    private lateinit var descriptionTextView: TextView
+    private lateinit var earthImageView: ImageView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,10 +36,24 @@ class EarthFragment : Fragment() {
 
         _binding = FragmentEarthBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        latitudeEditText = binding.editTextLatitude
+        longitudeEditText = binding.editTextLongitude
+        dateEditText = binding.editTextDate
+        fetchImageButton = binding.buttonFetchEarthImage
+        descriptionTextView = binding.textViewDescription
+        earthImageView = binding.imageViewEarth
 
         val textView: TextView = binding.textEarth
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+
+        fetchImageButton.setOnClickListener {
+
+            val latitude = latitudeEditText.text.toString()
+            val longitude = longitudeEditText.text.toString()
+            val date = dateEditText.text.toString()
+
         }
         return root
     }
--- app/src/main/java/com/example/terramars/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/terramars/ui/overview/OverviewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -11,6 +12,9 @@ import com.example.terramars.databinding.FragmentOverviewBinding
 
 class OverviewFragment : Fragment() {
 
+    private lateinit var imageViewOverview: ImageView
+    private lateinit var txtExplanation: TextView
+    private lateinit var txtDate: TextView
     private var _binding: FragmentOverviewBinding? = null
 
     // This property is only valid between onCreateView and
